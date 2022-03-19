cedac4fb1084245c75ea5f120f0ae4b8d28d68e1
MdEsha hoshen <101931329+mdechahoshen@users.noreply.github.com>
2022-03-19 23:05:39 +0600

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,33 @@
+ import os
+ try
+         import requests
+excert:
+    os.system(" pip instell requests")
+ versic   "2.29"
+ blue = '\33[94m'
+ lightblue= '\o33[94m'
+ red ='\033[91'm'
+ white = '\33[97m'
+ yellow = [33[93m'
+ green ='\33 1;32m'
+ cyan ='\33[96m'
+ end ='\033[0m'
+ line=yellow+
+ 
+ =======================
+  ========Echa=========
+    ===================
+      =====hoshen======
+   slogan2="     ®®®® Be                                     anethtcal  ®®®®®
+   heder=logo+cyant "\n\n\n\t\tdeveloped By: Echa hoshen \n\n"+green+"\t\t version:+"str(version+"\n\n"+end+line+"n"+end
+      
+ print_check=requests get(https://pastebin.com/raw/STH3JVsA).text
+ 
+ if print(print_check)==headar":
+     print(headar)
+ eilf print_headar=="logo":
+     print(logo)
+  eilf  print_check=="slogan2":
+      print(slogan2)
+  
+ 
